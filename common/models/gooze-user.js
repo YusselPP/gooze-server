2463d8102b2af448a5972c376dd503e5b1f81fed
Yussel Paredes <yparedes@tibco-support.com>
2017-10-20 17:08:02 -0500

    Defined GoozeUser properties

--- common/models/gooze-user.js
+++ common/models/gooze-user.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ *
+ * @param Goozeuser{Validatable}
+ */
 module.exports = function(Goozeuser) {
-
+  Goozeuser.validatesInclusionOf('gender', {in: ['male', 'female', 'other'], allowNull: true});
+  Goozeuser.validatesInclusionOf('status', {in: ['available', 'unavailable']});
+  Goozeuser.validatesInclusionOf('mode', {in: ['gooze', 'client']});
 };
