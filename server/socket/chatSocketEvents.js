ec4c4a8e8340febf383818e22d02b642ad2d3405
YusselPP <yussel_paredes@hotmail.com>
2018-03-31 18:14:43 -0600

    Chat socket: Added "sendMessage" event handler

--- server/socket/chatSocketEvents.js
+++ server/socket/chatSocketEvents.js
@@ -2,6 +2,16 @@
 
 var debug = require('debug')('gooze:chat-socket-events');
 
+var events = {
+  sendMessage: 'sendMessage',
+  messageReceived: 'messageReceived',
+};
+
 module.exports = function addChatSocketEvents(socket, clients, app) {
 
+  socket.on(events.sendMessage, function(data, callback) {
+    debug('sendMessage - event received');
+    debug(JSON.stringify(data));
+    callback();
+  }
 };
