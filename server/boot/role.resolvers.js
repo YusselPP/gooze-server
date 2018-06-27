92bd26e7c0f6585299b20cddf0b2b7238b53a38f
Yussel Paredes <yparedes@tibco-support.com>
2018-06-27 05:19:18 -0500

    Added chatOwner permissions resolver. Made public updateAll and count remote methods to handle Message read status

--- server/boot/role.resolvers.js
+++ server/boot/role.resolvers.js
@@ -51,4 +51,51 @@ module.exports = function(app) {
       }
     );
   });
+
+  Role.registerResolver('chatOwners', function(role, ctx, cb) {
+    var error;
+    function reject(err) {
+      process.nextTick(function() {
+        cb(err, false);
+      });
+    }
+
+    if (ctx.modelName !== 'ChatMessage') {
+      return reject(new Error('Invalid usage of chatOwners role resolver on ' + ctx.modelName + ' model.'));
+    }
+
+    var userId = ctx.accessToken.userId;
+    if (!userId) {
+      error = new Error('Authorization required.');
+      error.statusCode = 401;
+      return reject(error);
+    }
+
+    var chatId = ctx.remotingContext.args.where && ctx.remotingContext.args.where.chatId;
+    debug('chatId: ' + chatId);
+    if (!chatId) {
+      error = new Error('Missing required parameter: chatId.');
+      error.statusCode = 400;
+      return reject(error);
+    }
+
+    app.models.Chat.count(
+      {
+        id: chatId,
+        or: [
+          {user1Id: userId},
+          {user2Id: userId}
+        ]
+      },
+      function(err, count) {
+        debug('Count: ' + count);
+        if (err) {
+          console.error(err);
+          return cb(err, false);
+        }
+
+        return cb(null, count > 0);
+      }
+    );
+  });
 };
